380ad85fe96944792ba1edd1a7648aa1015d10f1
Magnus <magnus@muellerjena.de>
2019-01-02 11:14:45 +0100

    Initial Commit

---
+++ destroy-the-world/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  destroy-the-world
+//
+//  Created by Magnus Müller on 02.01.19.
+//  Copyright © 2019 Magnus Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
